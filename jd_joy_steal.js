b8db85ab5b851233e875571afc93900e4d7a465d
DouBa <viphsj@gmail.com>
2021-02-22 11:27:53 +0800

    Update jd_joy_steal.js

--- jd_joy_steal.js
+++ jd_joy_steal.js
@@ -631,6 +631,7 @@ function timeFormat(time) {
   } else {
     date = new Date();
   }
+  console.log(date)
   return date.getFullYear() + '-' + ((date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1)) + '-' + (date.getDate() >= 10 ? date.getDate() : '0' + date.getDate());
 }
 function jsonParse(str) {
