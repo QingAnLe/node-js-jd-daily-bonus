a56b1e716c6bb1f85d7b6e6b93e1b016de36409e
DouBa <viphsj@gmail.com>
2021-02-24 17:26:40 +0800

    Update jdSgmhShareCode.js

--- jdSgmhShareCode.js
+++ jdSgmhShareCode.js
@@ -15,7 +15,6 @@ if (process.env.SGMHSHARECODES) {
 } else if (process.env.JD_COOKIE) {
   console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
-console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 for (let i = 0; i < inviteCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['inviteCodes' + index] = inviteCodes[i];
