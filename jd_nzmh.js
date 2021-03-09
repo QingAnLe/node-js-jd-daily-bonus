f0a9d516c282b833d569e216ab81bf882eaed343
DouBa <viphsj@gmail.com>
2021-03-09 22:10:10 +0800

    Update jd_nzmh.js

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -61,9 +61,10 @@ if ($.isNode()) {
         }
         continue
       }
-      await jdMh()
-      await jdMh('https://anmp.jd.com/babelDiy/Zeus/gY7ymUmC8ZM74Zw3woiDDQU1naT/index.html?wxAppName=jd')
-      await jdMh('https://anmp.jd.com/babelDiy/Zeus/3UGPT8RMBu4kL2YAYN98MgkcDhRq/index.html?wxAppName=jd')
+      await jdMh('https://anmp.jd.com/babelDiy/Zeus/4J7phaGgkAAq7jwnhnodtqfit6M8/index.html')
+      // await jdMh('https://anmp.jd.com/babelDiy/Zeus/gY7ymUmC8ZM74Zw3woiDDQU1naT/index.html?wxAppName=jd')
+      // await jdMh('https://anmp.jd.com/babelDiy/Zeus/3UGPT8RMBu4kL2YAYN98MgkcDhRq/index.html?wxAppName=jd')
+      // await jdMh('https://anmp.jd.com/babelDiy/Zeus/yiNQjMxQvs3R3SdS4nwa2MFk1FE/index.html?wxAppName=jd')
     }
   }
 })()
