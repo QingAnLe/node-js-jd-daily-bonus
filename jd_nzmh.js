a439173824c14825b87b63d56c68346d44425e77
DouBa <viphsj@gmail.com>
2021-05-31 09:29:19 +0800

    Update jd_nzmh.js

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -62,6 +62,7 @@ if ($.isNode()) {
       }
       try {
         await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3eeruLXVbXge6CexVq8XkBbBvAfy/index.html')
+        await jdMh('https://h5.m.jd.com/babelDiy/Zeus/2k7Tx67WzLHDtdDNB2KPnqWEwavX/index.html')
         // await jdMh('https://anmp.jd.com/babelDiy/Zeus/3UGPT8RMBu4kL2YAYN98MgkcDhRq/index.html?wxAppName=jd')
         // await jdMh('https://anmp.jd.com/babelDiy/Zeus/yiNQjMxQvs3R3SdS4nwa2MFk1FE/index.html?wxAppName=jd')
       } catch (e) {
